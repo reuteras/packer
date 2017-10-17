e4d70af05cfbcd07d791837ebc94cafd344fc02b
Peter Reuterås <peter@reuteras.com>
2017-10-17 22:45:00 +0200

    Install tools for basic Debian.

---
+++ packer-debian/scripts/setup-debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux                                                                                                                                                                                                                              
+sudo apt-get -y -qq install \
+    crudini \
+    ctags \
+    curl \
+    exfat-fuse \
+    exfat-utils \
+    git \
+    vim \
+    vim-doc \
+    vim-scripts \
+    zip
+
+sudo apt-get -y -qq install open-vm-tools-desktop fuse
+
