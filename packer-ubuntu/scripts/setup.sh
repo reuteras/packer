7a44748d86308297e9810f6f704e1d7faec6d021
Peter Reuterås <peter@reuteras.com>
2018-12-16 21:50:36 +0100

    Make sure vim is installed.

--- packer-ubuntu/scripts/setup.sh
+++ packer-ubuntu/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 # Install git
 apt-get -yqq update
-apt-get -yqq install git open-vm-tools open-vm-tools-desktop
+apt-get -yqq install git open-vm-tools open-vm-tools-desktop vim
 
 # Add malware user to sudoers.
 echo "malware        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
