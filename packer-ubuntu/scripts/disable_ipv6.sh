b718f54260eceb0d0a575a0a9bb4bf81598e118f
Peter Reuterås <peter@reuteras.com>
2020-01-08 16:10:32 +0100

    Disable ipv6

---
+++ packer-ubuntu/scripts/disable_ipv6.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eux
+
+sudo sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="maybe-ubiquity"/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 maybe-ubiquity"/' /etc/default/grub
+sudo sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' /etc/default/grub
+sudo update-grub
