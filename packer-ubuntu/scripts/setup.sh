cbd8d0f71924a8e8edb80c86876adcd49a9c048d
Peter Reuterås <peter@reuteras.com>
2020-03-29 19:04:07 +0200

    Run dist-upgrade during install.

--- packer-ubuntu/scripts/setup.sh
+++ packer-ubuntu/scripts/setup.sh
@@ -3,6 +3,7 @@
 # Install git
 apt-get -yqq update
 apt-get -yqq install git open-vm-tools open-vm-tools-desktop vim
+apt-get -yqq dist-upgrade
 
 # Add malware user to sudoers.
 echo "malware        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
