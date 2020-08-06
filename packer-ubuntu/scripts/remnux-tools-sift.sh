08d8b4fd1010ed398c7ecf8de7acf2325cfe6f89
Peter Reuterås <peter@reuteras.com>
2020-08-06 09:35:15 +0200

    Print more progress info.

--- packer-ubuntu/scripts/remnux-tools-sift.sh
+++ packer-ubuntu/scripts/remnux-tools-sift.sh
@@ -2,6 +2,9 @@
 
 git clone https://github.com/reuteras/remnux-tools.git
 cd remnux-tools
+echo "## Run: ./bin/setup-sift.sh"
 ./bin/setup-sift.sh
+echo "## Run: make dotfiles"
 make dotfiles
+echo "## Run: ./bin/install-vmhgfs.sh ~/linux.iso"
 ./bin/install-vmhgfs.sh ~/linux.iso
