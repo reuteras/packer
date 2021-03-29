6c081c0f9869cff9a430f6bd3570e84b110d92eb
Peter Reuterås <peter@reuteras.com>
2021-03-29 11:55:55 +0200

    Script for virt

---
+++ packer-debian/scripts/setup-virt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+sudo apt -y -qq install \
+    qemu-system-x86_64 \
+    virt-manager
+
+sudo adduser cuckoo libvirt
+
+# Packer
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update && sudo apt -y -qq install packer
+
+cd || exit
+git clone https://github.com/reuteras/packer.git
+cd packer || exit
+wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso
