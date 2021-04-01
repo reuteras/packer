c12e058b7eaf33203b2302c580e829b564349e8e
Peter Reuterås <peter@reuteras.com>
2021-04-01 14:53:48 +0200

    Add script for small xfce with chrome install

---
+++ packer-debian/scripts/setup-debian-chrome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eux                                                                                                                                                                                                                              
+sudo apt-get -y -qq install \
+    curl \
+    open-vm-tools-desktop \
+    vim \
+    vim-doc \
+    vim-scripts \
+    wget
+
+cd /tmp || exit
+wget -q -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# shellcheck disable=SC2024
+sudo dpkg -i google-chrome-stable_current_amd64.deb 
+# shellcheck disable=SC2024
+sudo apt -qq -f -y install
+rm -f google-chrome-stable_current_amd64.deb
