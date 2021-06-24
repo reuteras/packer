75c8616afa3d2f6a3b49847a37176e2c85fa1d6c
Peter Reuterås <peter@reuteras.com>
2021-06-24 12:46:22 +0200

    Add target and config for Timesketch.

---
+++ packer-ubuntu/scripts/install_timesketch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eux
+
+# Install Docker
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-compose
+
+# Install Timesketch
+curl -s -O https://raw.githubusercontent.com/google/timesketch/master/contrib/deploy_timesketch.sh
+chmod 755 deploy_timesketch.sh
+cd /opt
+sudo ~/deploy_timesketch.sh
+cd timesketch
+sudo docker-compose pull
+
+# Download plaso
+sudo docker pull log2timeline/plaso
