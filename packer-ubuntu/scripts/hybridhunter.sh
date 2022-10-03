f46ebc1a157868b9d0fc905e900d75c46cb644a6
Peter Reuterås <peter@reuteras.net>
2022-10-03 14:13:30 +0200

    Remove unused code.

--- packer-ubuntu/scripts/hybridhunter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p "$HOME"/Documents/src || exit
-cd "$HOME"/Documents/src || exit
-git clone https://github.com/Security-Onion-Solutions/securityonion-saltstack
---
+++ packer-ubuntu/scripts/malcolm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/malware || exit
+git clone https://github.com/reuteras/malir.git
+cd malir || exit
+./install.sh
