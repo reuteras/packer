99bcede90d2c016b4bb9ae92670099028c471b97
Peter Reuterås <peter@reuteras.com>
2023-02-03 06:09:47 +0100

    No grub-gfxpayload-lists on aarch64.

--- packer-ubuntu/scripts/gui.sh
+++ packer-ubuntu/scripts/gui.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo grub-gfxpayload-lists firefox
+if [[ $(uname -m) == "aarch64" ]]; then
+    apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo firefox
+else
+    apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo grub-gfxpayload-lists firefox
+fi
+
