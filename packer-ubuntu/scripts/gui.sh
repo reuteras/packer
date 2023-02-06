65190f1d094d3a6bdf0974469c1600d539bc8acd
Peter Reuterås <peter@reuteras.com>
2023-02-06 10:22:05 +0100

    Revert change. Good to see activity

--- packer-ubuntu/scripts/gui.sh
+++ packer-ubuntu/scripts/gui.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $(uname -m) == "aarch64" ]]; then
-    apt-get install -yqq ubuntu-desktop plymouth-theme-ubuntu-logo firefox
+    apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo firefox
 else
-    apt-get install -yqq ubuntu-desktop plymouth-theme-ubuntu-logo grub-gfxpayload-lists firefox
+    apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo grub-gfxpayload-lists firefox
 fi
 
