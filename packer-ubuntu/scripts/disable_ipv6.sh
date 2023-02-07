268630eb8d10cf391cb5b9b8e3f55dc38431b0db
Peter Reuterås <peter@reuteras.com>
2023-02-07 07:35:57 +0100

    Fix disable of IPv6

--- packer-ubuntu/scripts/disable_ipv6.sh
+++ packer-ubuntu/scripts/disable_ipv6.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
 sudo sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="maybe-ubiquity"/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 maybe-ubiquity"/' /etc/default/grub
+sudo sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="autoinstall/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 autoinstall/' /etc/default/grub
 sudo sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' /etc/default/grub
 sudo update-grub
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
