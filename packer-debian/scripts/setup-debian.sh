39fb531d3650277c4578bbbcc50c05edc79caf05
Peter Reuterås <peter@reuteras.com>
2023-08-04 13:50:29 +0200

    Update

--- packer-debian/scripts/setup-debian.sh
+++ packer-debian/scripts/setup-debian.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eux                                                                                                                                                                                                                              
-#sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install \
-#    curl \
-#    fuse \
-#    exfat-fuse \
-#    git \
-#    vim \
-#    vim-doc \
-#    vim-scripts \
-#    zip
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install \
+    curl \
+    git \
+    vim \
+    vim-doc \
+    vim-scripts \
+    zip \
+    fuse \
+    exfat-fuse
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install open-vm-tools-desktop
 
