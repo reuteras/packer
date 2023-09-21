21d764fca5215397f70c277641583004e449aba7
Peter Reuterås <peter@reuteras.com>
2023-09-21 11:00:55 +0200

    Don't install some packages

--- packer-debian/scripts/setup-debian.sh
+++ packer-debian/scripts/setup-debian.sh
@@ -3,11 +3,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install \
     curl \
     git \
     vim \
-    vim-doc \
     vim-scripts \
-    zip \
-    fuse \
-    exfat-fuse
+    zip
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install open-vm-tools-desktop
 
