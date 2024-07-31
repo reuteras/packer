20fe3830ffebf6f03eca4dffba24d92213427a1b
Coding for fun <peter@reuteras.net>
2024-07-31 09:17:41 +0200

    Fix for Ubuntu 24.04 LTS

--- packer-ubuntu/scripts/gui.sh
+++ packer-ubuntu/scripts/gui.sh
@@ -3,6 +3,10 @@
 if [[ $(uname -m) == "aarch64" ]]; then
     apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo firefox
 else
-    apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo grub-gfxpayload-lists firefox
+    if grep "trixie/sid" /etc/debian_version > /dev/null ; then
+        apt-get install -y ubuntu-desktop grub-gfxpayload-lists firefox
+    else
+        apt-get install -y ubuntu-desktop plymouth-theme-ubuntu-logo grub-gfxpayload-lists firefox
+    fi
 fi
 
